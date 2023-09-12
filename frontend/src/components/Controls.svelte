e58e86587c18a122212471c6f9da39f8ebc3dff8
Viktor <ngovuong0987@gmail.com>
2023-09-12 10:09:51 +0800

    Add nicer UI, clean up backend

--- frontend/src/components/Controls.svelte
+++ frontend/src/components/Controls.svelte
@@ -84,7 +84,7 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    gap: 10px;
+    gap: 11px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   }
   .progress {
